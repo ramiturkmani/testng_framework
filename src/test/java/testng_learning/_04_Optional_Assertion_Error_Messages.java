e1137dcf930ecd14d4da6cfd3427f556328e63bd
ramiturkmani <ramiturkmani1@gmail.com>
2022-06-02 22:29:00 -0500

    added JUnit

---
+++ src/test/java/testng_learning/_04_Optional_Assertion_Error_Messages.java
@@ -0,0 +1,21 @@
+package testng_learning;
+
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+public class _04_Optional_Assertion_Error_Messages {
+
+    /*
+    @Test
+    public void method1(){
+        //TestNG way
+        //Assert.assertTrue("False", "Error Message");
+        org.testng.Assert.assertEquals("actual", "expected", "message");
+
+        //JUnit way
+        Assert.assertTrue("message", false);
+        Assert.assertEquals("actual", "expected", "message");
+    }
+     */
+
+}
